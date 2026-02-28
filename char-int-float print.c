397bf2d25303692fd62f7cb65b75b70bb31e6a2f
unknown <yeasinmiah1272001@gmail.com>
2026-02-28 10:17:29 +0600

    Assignmnet-1

---
+++ char-int-float print.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int age = 21;
+    float height = 5.9;
+    char grade = 'A';
+
+    printf("Age: %d\n Height : %1f\n Grade: %c\n", age,height,grade);
+    return 0;
+}
